d366b87dbfa99f54b23b7338e83f00c2f8243371
Rodney-Willems <rodneywillems@gmail.com>
2024-09-26 12:34:07 +0200

    Cleaned up code

--- Assets/SetRotation.cs
+++ Assets/SetRotation.cs
@@ -6,9 +6,11 @@ public class SetRotation : MonoBehaviour
 {
     void Update()
     {
+        // The box outline follows the mouse within a certain range
         Vector3 mouse = Camera.main.ScreenToWorldPoint( Input.mousePosition );
-
-        transform.rotation = Quaternion.Euler(0, 0, 0);
         transform.localPosition = new Vector2(Mathf.Clamp(mouse.x, -2, 2), Mathf.Clamp(mouse.y, -2, 2));
+
+        // The box outline always stays rotated upright
+        transform.rotation = Quaternion.Euler(0, 0, 0);
     }
 }
