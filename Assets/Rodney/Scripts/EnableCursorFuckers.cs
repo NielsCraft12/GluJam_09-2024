f9d5e803fbbbf23dbc0deeac8fdf995bb4fa46d0
Rodney-Willems <rodneywillems@gmail.com>
2024-09-26 22:15:48 +0200

    Added EnableCursorFuckers

---
+++ Assets/Rodney/Scripts/EnableCursorFuckers.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableCursorFuckers : MonoBehaviour
+{
+    void Start()
+    {
+        // Bestaat LETTERLIJK alleen om de fucking Cursor te laten zien in andere scenes
+        Cursor.visible = true; 
+    }
+}
